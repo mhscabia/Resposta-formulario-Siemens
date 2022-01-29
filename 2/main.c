0c2f536b023c07dc62b441ad5022aaaa691e9eb7
Matheus Scabia <67128521+mhscabia@users.noreply.github.com>
2022-01-29 09:58:22 -0300

    Update main.c

--- 2/main.c
+++ 2/main.c
@@ -5,7 +5,7 @@ int main(){
 char num[6]; //limite de 6 por que a maior palavra é FooBaa, de 6 letras
 
 for (int i = 1; i <= 100; i++){
-    sprintf(num, "%i", i);
+    sprintf(num, "%i", i); //para evitar usar dois prints, utilizei o sprintf para guardar o "i" como string
     printf("%s\n", i % 3 == 0 ? (i % 5 == 0 ? "FooBaa" : "Foo") : (i % 5 == 0 ? "Baa" : num));
 }
 
